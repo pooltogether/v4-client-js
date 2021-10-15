fa21a927c7ca79f544a79a269478e139ccc8bfb0
Charles Bergeron <chuck.bergeron@gmail.com>
2021-10-15 09:15:30 -0700

    * higher depositTo gasLimit

--- src/Player.ts
+++ src/Player.ts
@@ -65,7 +65,7 @@ export class Player extends PrizePool {
     const errorPrefix = 'Player [depositTo] | '
     await this.validateSignerNetwork(errorPrefix)
 
-    const overrides = { gasLimit: 275000 }
+    const overrides = { gasLimit: 350000 }
 
     const usersAddress = await this.signer.getAddress()
     return this.prizePoolContract.depositTo(usersAddress, amount, overrides)
