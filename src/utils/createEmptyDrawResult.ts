2a5b422b792a376577adf9be5b26ea8f46ef9769
Dylan DesRosier <dylan@pooltogether.com>
2022-02-18 16:10:18 -0500

    Add type to empty draw result

--- src/utils/createEmptyDrawResult.ts
+++ src/utils/createEmptyDrawResult.ts
@@ -1,6 +1,7 @@
+import { DrawResults } from '@pooltogether/v4-utils-js'
 import { BigNumber } from 'ethers'
 
-export const createEmptyDrawResult = (drawId: number) => ({
+export const createEmptyDrawResult = (drawId: number): DrawResults => ({
   drawId,
   totalValue: BigNumber.from(0),
   prizes: []
